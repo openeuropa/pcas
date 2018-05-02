770fe68c1a20f3bd995b02c2510708e38a6902f3
Pol Dellaiera <pol.dellaiera@protonmail.com>
2018-05-02 16:31:47 +0200

    Issue #2: Don't use DI container for non-singleton objects.

--- src/Utils/PCasSerializerFactory.php
+++ src/Utils/PCasSerializerFactory.php
@@ -7,7 +7,7 @@ use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
-class PCasSerializerFactory
+class PCasSerializerFactory implements PCasSerializerFactoryInterface
 {
     public function createSerializer()
     {
